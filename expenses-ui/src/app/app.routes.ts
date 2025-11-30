37d06868803e9b0550d2a26d8634d340de6906ce
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2025-11-29 20:24:33 -0500

    feat: Add routing, integrate Angular Material with a custom theme, and set up a basic navigation bar.

--- expenses-ui/src/app/app.routes.ts
+++ expenses-ui/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './route/home/home';
+import { Manage } from './route/manage/manage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'manage',
+        component: Manage,
+    },
+];
--- expenses-ui/src/app/common/navbar/navbar.ts
+++ expenses-ui/src/app/common/navbar/navbar.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+    iconRegistry.addSvgIcon(
+      'atm',
+      sanitizer.bypassSecurityTrustResourceUrl('local-atm.svg')
+    );
+  }
 }
